6072644d5687ee94869e372ce93d45b65e2a9400
Nafi-GOAT <nafih2989@gmail.com>
2025-10-05 21:54:53 -0700

    new one

--- Lab 19/main.cpp
+++ Lab 19/main.cpp
@@ -80,6 +80,7 @@ float ran() {
     int r = rand() % 41 + 10;
     return r / 10.0f;
 }
+
 int main() {
     srand(time(0));
 
@@ -89,33 +90,4 @@ int main() {
     movies.emplace_back("Titanic");
     movies.emplace_back("The Notebook");
 
-    ifstream file("review.txt");
-    if (!file) {
-        cerr << "Could not open review.txt\n";
-        return 1;
-    }
-
-    vector<string> comments;
-    string line;
-    while (getline(file, line)) {
-        if (!line.empty()) {
-            comments.push_back(line);
-        }
-    }
-    file.close();
-
-    int num = 0;
-    for (auto &movie : movies) {
-        for (int i = 0; i < 3 && num < (int)comments.size(); i++) {
-            float rating = ran();
-            movie.review(rating, comments[num]);
-            num++;
-        }
-    }
-
-    for (auto &movie : movies)
-        movie.output();
-
-    return 0;
-}
 
